fabe32460090ad66cc88534b59e8fa1603dbc5c4
thofreising141@gmail.com <englishisno1!>
2018-10-17 16:40:45 +0200

    add parameterservlet class

---
+++ src/main/java/de/hanel/jee/ParameterServlet.java
@@ -0,0 +1,20 @@
+package de.hanel.jee;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.core.MediaType;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class ParameterServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType(MediaType.TEXT_HTML);
+        resp.setCharacterEncoding("utf-8");
+
+        PrintWriter writer = resp.getWriter();
+        writer.println("<html><head><title>Parameter Servlet</title><body><h1 style=\"color: orange;\">Parameter</h1></body></head></html>");
+    }
+}
